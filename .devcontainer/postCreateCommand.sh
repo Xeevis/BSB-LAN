00720a910ddf71467fbadce4d680cb9b8f328430
Xeevis <Xeevis@users.noreply.github.com>
2025-08-14 23:57:16 +0200

    🎨 Code cleanup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
-sudo mkdir -p bin
-sudo chown vscode bin
+#!/bin/bash
 cp --update=none BSB_LAN/BSB_LAN_config.h.default BSB_LAN/BSB_LAN_config.h  # No overwrite
 cp --update=none BSB_LAN/BSB_LAN_custom_defs.h.default BSB_LAN/BSB_LAN_custom_defs.h  # No overwrite
+sudo chown -R vscode:vscode bin
 sudo mkdir -p /home/vscode/.arduino15 && sudo chown -R vscode:vscode /home/vscode/.arduino15
 arduino-cli config init --overwrite \
   ; arduino-cli config set board_manager.additional_urls "https://espressif.github.io/arduino-esp32/package_esp32_index.json" \
