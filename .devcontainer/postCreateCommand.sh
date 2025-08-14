a36328ddd67eeb5b0fff3f8b0540c8d02dfa259a
Xeevis <Xeevis@users.noreply.github.com>
2025-08-14 23:55:18 +0200

    ♻️ Move Arduino CLI files from docker image into persistent volume

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,10 @@ sudo mkdir -p bin
 sudo chown vscode bin
 cp --update=none BSB_LAN/BSB_LAN_config.h.default BSB_LAN/BSB_LAN_config.h  # No overwrite
 cp --update=none BSB_LAN/BSB_LAN_custom_defs.h.default BSB_LAN/BSB_LAN_custom_defs.h  # No overwrite
+sudo mkdir -p /home/vscode/.arduino15 && sudo chown -R vscode:vscode /home/vscode/.arduino15
+arduino-cli config init --overwrite \
+  ; arduino-cli config set board_manager.additional_urls "https://espressif.github.io/arduino-esp32/package_esp32_index.json" \
+  ; arduino-cli core update-index \
+  ; arduino-cli core install esp32:esp32 \
+  ; arduino-cli core install arduino:sam \
+  ; arduino-cli lib update-index
